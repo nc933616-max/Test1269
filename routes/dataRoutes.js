e6bc2b93f33f787cb1bc2ec7a01ccec3f503e70f
nc933616-max <nc933616@gmail.com>
2026-01-12 14:24:06 +0530

    Add data routes for CRUD operations

---
+++ routes/dataRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getAllData,
+  getDataById,
+  createData,
+  updateData,
+  deleteData
+} from "../dataController.js";
+
+const router = express.Router();
+
+router.get("/", getAllData);
+router.get("/:id", getDataById);
+router.post("/", createData);
+router.put("/:id", updateData);
+router.delete("/:id", deleteData);
+
+export default router;
